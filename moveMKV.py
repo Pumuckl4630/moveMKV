518cbd47ecbe8c9a012f8265ce4bfa11383b191e
sakatoshi <julian-reza@proton.me>
2024-03-14 10:46:58 +0100

    .py

---
+++ moveMKV.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+def move_mkv_files(source_dir, destination_dir):
+    # Überprüfe, ob das Zielverzeichnis existiert, andernfalls erstellen
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
+    # Durchsuche des Quellverzeichnisses und seiner Unterordner
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith('.mkv'):
+                source_file_path = os.path.join(root, file)
+                destination_file_path = os.path.join(destination_dir, file)
+                # Überprüfe, ob die Datei bereits existiert, und sie ggf. umbenennen
+                if os.path.exists(destination_file_path):
+                    filename, file_extension = os.path.splitext(file)
+                    i = 1
+                    while os.path.exists(os.path.join(destination_dir, f"{filename}_{i}{file_extension}")):
+                        i += 1
+                    destination_file_path = os.path.join(destination_dir, f"{filename}_{i}{file_extension}")
+                # Verschieben der Datei in das Zielverzeichnis
+                shutil.move(source_file_path, destination_file_path)
+                print(f"Moved {file} to {destination_file_path}")
+
+# Beispielaufruf
+source_directory = '/home/sakatoshi/Schreibtisch/DragonBall/DragonBall Z SD'
+destination_directory = '/home/sakatoshi/Schreibtisch/DragonBall/DragonBall Z SD/DragonBall Z'
+move_mkv_files(source_directory, destination_directory)
